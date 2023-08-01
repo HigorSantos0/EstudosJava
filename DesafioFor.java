04538b67e8f59ab3fe40b6ac92394eac0231273c
Higor do Nascimento Santos <higor.2120478300029@faeterj-rio.edu.br>
2023-08-01 16:21:45 -0300

    Add files via upload

---
+++ DesafioFor.java
@@ -0,0 +1,17 @@
+package Controle;
+
+public class DesafioFor {
+
+	public static void main(String[] args) {
+		
+		String valor2 = "######";
+		
+		String valor;
+		
+		for( valor = "#";!valor.equals(valor2);valor += "#")
+		{
+			System.out.println(valor);
+		}
+		
+	}
+}
